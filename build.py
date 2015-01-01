bf862115f8f3d767d43e36adef3bb260c6bc223d
Kunal Mehta <legoktm@gmail.com>
2015-01-01 15:52:04 -0800

    Specify utf-8 charset

--- build.py
+++ build.py
@@ -33,6 +33,7 @@ def get_all_albums():
 html = """
 <html>
     <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>swift-player</title>
         <script src="js/jquery-2.1.1.min.js"></script>
         <script src="js/playlist.js"></script>
